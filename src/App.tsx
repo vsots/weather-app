73442ce543628cec277dcc2e19a0dbd1ac2e7d47
vsots <vsotsdev@gmail.com>
2023-11-17 20:23:50 -0800

    feat(weather-app): Adds useQuery and weather api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import fetchWeather from './fetchWeather';
+
 import './App.css'
 
 function App() {
+  const [requestParams, setRequestParams] = useState("");
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["search", requestParams], 
+    queryFn: fetchWeather
+  });
 
   return (
     <>
       <div className="input">
         <h1>Find Your Weather</h1>
-        <input type="text" readOnly={false} placeholder="Location"></input>
-        <button>Search</button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault(); // Do not want to do full form submit as that refreshes page
+            // requestPets();
+            const formData = new FormData(e.currentTarget); // browser api, not react
+            // const obj = {
+            //   location: formData.get("location") as string ?? ""
+            // };
+            setRequestParams(formData.get("location") as string ?? "Novato");
+          }}
+        >
+          <label htmlFor="location">
+            <input
+              type="text"
+              name="location"
+              id="location"
+              placeholder="Location"
+              readOnly={false}
+            />
+          </label>
+          <button>Search</button>
+        </form>
       </div>
+      {isSuccess ? <div>{data?.timelines?.daily[0]?.time}</div> : <div>No Results</div>}
     </>
   )
 }
---
+++ src/fetchWeather.ts
@@ -0,0 +1,13 @@
+async function fetchWeather({ queryKey }) {
+  const location = queryKey[1];
+  const API_KEY = "YOUR_TOMORROW.IO_API_KEY"
+  const res = await fetch(
+    `https://api.tomorrow.io/v4/weather/forecast?location=${location}&apikey=${API_KEY}`
+  );
+  if (!res.ok)
+    throw new Error(`weather search failed for: ${location}`);
+
+  return res.json();
+}
+
+export default fetchWeather;
