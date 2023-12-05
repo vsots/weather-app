070a7762568d91cefaa80c8205f5bf41a658246e
vsots <vsotsdev@gmail.com>
2023-12-05 09:45:58 -0800

    refactor(weather-app): edits default path on search

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useQuery } from '@tanstack/react-query';
 import fetchWeather from './fetchWeather';
 import WeatherData from './WeatherData';
@@ -39,15 +39,16 @@ function App() {
         {isSuccess ? (
           <div>
             <h2>Weather for {data?.location?.name}</h2>
-            <Link to="/"> Clear </Link>
             <Link to="/daily"> Daily </Link>
             <Link to="/hourly"> Hourly </Link>
             <Link to="/minutely"> Minutely </Link>
             <Routes>
-              <Route path="/" element={<></>} />
-              <Route path="/daily" element={<WeatherData data={data?.timelines?.daily} type={"Daily"} />} />
-              <Route path="/hourly" element={<WeatherData data={data?.timelines?.hourly} type={"Hourly"} />} />
-              <Route path="/minutely" element={<WeatherData data={data?.timelines?.minutely} type={"Minutely"} />} />
+              <Route path="/">
+                <Route index element={<Navigate to="/daily" />} />
+                <Route path="daily" element={<WeatherData data={data?.timelines?.daily} type={"Daily"} />} />
+                <Route path="hourly" element={<WeatherData data={data?.timelines?.hourly} type={"Hourly"} />} />
+                <Route path="minutely" element={<WeatherData data={data?.timelines?.minutely} type={"Minutely"} />} />
+              </Route>  
             </Routes>
           </div>
         ) : <div>No Results</div>}
