8dffbadbeecbe5b70e73e1eaa014707bd85aadb6
vsots <vsotsdev@gmail.com>
2023-12-09 09:29:02 -0800

    feat(weather-app): adds css-grid

--- src/WeatherData.tsx
+++ src/WeatherData.tsx
@@ -5,14 +5,14 @@ function WeatherData({ type, data }: WeatherDataProps) {
     <>
       {
         data ? ( 
-          <div>
+          <div className="forecast">
             <h3>Your {type} Forecast</h3>
-            <div className="row">
+            <div className="weather-grid">
               {
                 data.map((item) => {
                   const date = new Date(item.time).toString().split(' ');
                   return (
-                    <div className="column" key={item.time}>
+                    <div className="weather-item" key={item.time}>
                       <p>{date.slice(0, 4).join(' ')}</p>
                       <p>{date[4]} {date.slice(6).join(' ')}</p>
                       <p>{item.values.temperature ?? item.values.temperatureMax} Degrees Celcius</p>
